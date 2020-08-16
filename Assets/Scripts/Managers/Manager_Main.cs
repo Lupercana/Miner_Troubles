da5e6b046d65fdb8ec99b96e910568148f4bc90f
Bo <zboyang@umich.edu>
2020-08-15 22:02:33 -0400

    UI gems

---
+++ Assets/Scripts/Managers/Manager_Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager_Main : MonoBehaviour
+{
+    [SerializeField] private Image[] ui_gems = null;
+
+    [SerializeField] private Color[] gem_colors = null;
+
+    private void Start()
+    {
+        // Set gem colors in UI
+        for (int i = 0; i < ui_gems.Length; ++i)
+        {
+            ui_gems[i].color = gem_colors[i];
+        }
+    }
+}
